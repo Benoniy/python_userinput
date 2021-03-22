b7d0d104ccb93769fec80cc7efa56bcde70b540e
Ben <benranson5@gmail.com>
2021-03-22 15:45:12 +0000

    Added the completed Trello user input task

---
+++ python_task.py
@@ -0,0 +1,15 @@
+# Trello Task
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+age = input("Please enter your age: ")
+salary = input("Please enter your salary: ")
+DOB = input("Please enter your date of birth: ")
+post_code = input("Please enter your post code: ")
+
+
+print(first_name, "is", type(first_name))
+print(last_name, "is", type(last_name))
+print(age, "is", type(age))
+print(salary, "is", type(salary))
+print(DOB, "is", type(DOB))
+print(post_code, "is", type(post_code))
